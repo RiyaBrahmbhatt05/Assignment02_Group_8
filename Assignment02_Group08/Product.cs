35637d2a59b6dae8d65262a72dff8098e93a07e4
RiyaBrahmbhatt05 <rbrahmbhatt9551@conestogac.on.ca>
2025-02-03 21:23:10 -0500

    Initial setup and created Product.cs

---
+++ Assignment02_Group08/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_Group08
+{
+    public class Product
+    {
+        // Attributes
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+        // Constructor
+        public Product(int prodID, string prodName, decimal itemPrice, int stockAmount)
+        {
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        // Methods to increase and decrease stock
+        public void IncreaseStock(int amount)
+        {
+            if (StockAmount + amount > 800000)
+            {
+                throw new InvalidOperationException("Stock amount cannot exceed the maximum limit of 800,000.");
+            }
+            StockAmount += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount to decrease stock cannot be negative.");
+            }
+
+            if (StockAmount - amount < 8)
+            {
+                throw new InvalidOperationException("Stock amount cannot exceed the minimum limit of 8.");
+            }
+
+            StockAmount -= amount;
+        }
+
+    }
+}
---
+++ Assignment02_Group08/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assignment02_Group08")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Assignment02_Group08")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1115c172-cf80-4fd7-9939-1a15c7cf83b3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
